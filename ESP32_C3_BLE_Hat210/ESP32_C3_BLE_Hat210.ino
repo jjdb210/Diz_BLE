76ccf7481d9ed7b50bedc10cdf07b1f958ffbc70
jjdb210 <justin@jrcorps.com>
2025-07-20 11:04:04 -0500

    Some SMall Code Cleanup on V1

--- ESP32_C3_BLE_Hat210/ESP32_C3_BLE_Hat210.ino
+++ ESP32_C3_BLE_Hat210/ESP32_C3_BLE_Hat210.ino
@@ -1,79 +1,101 @@
-/*
-   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
-   Ported to Arduino ESP32 by Evandro Copercini
-   Changed to a beacon scanner to report iBeacon, EddystoneURL and EddystoneTLM beacons by beegee-tokyo
+/* 
+  Justins Hat210 V3.3 Code - https://emcot.world/The_Disney_Hat_(Hat210)
+  Designed for an ESP32c3 with SD Card Attachment
+  Author: Justin Gehring
+  http://emcot.world/
+  
+  Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+  Ported to Arduino ESP32 by Evandro Copercini
 */
 
 #include <Arduino.h>
-
 #include <Adafruit_NeoPixel.h>
 #include "NimBLEDevice.h"
 #include <NimBLEAdvertisedDevice.h>
 #include "NimBLEBeacon.h"
-
 #include <SPI.h>
 #include <SD.h>
-
-#include <ESP32Time.h>
-ESP32Time rtc(3600);
-
-
-const int chipSelect = 21;
-
-char savefilename[30] = "/datalog_00.txt";
-
-File dataFile;
 #include "FS.h"
+#include <ESP32Time.h>
+
 #define PIN D6
 #define N_LEDS 60
+#define ENDIAN_CHANGE_U16(x) ((((x)&0xFF00) >> 8) + (((x)&0xFF) << 8))
 
-/*
-#define CS 7
-#define MOSI 10
-#define MISO 6
-#define SCK 8
-*/
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c "
+#define BYTE_TO_BINARY(byte) \
+  (byte & 0x80 ? '1' : '0'), \
+    (byte & 0x40 ? '1' : '0'), \
+    (byte & 0x20 ? '1' : '0'), \
+    (byte & 0x10 ? '1' : '0'), \
+    (byte & 0x08 ? '1' : '0'), \
+    (byte & 0x04 ? '1' : '0'), \
+    (byte & 0x02 ? '1' : '0'), \
+    (byte & 0x01 ? '1' : '0')
+
+
+int scanTime = 1;  //In seconds
+int sequenceNumber = 0;
+int mode = 1;  
+int dressmode = 0; //Default to Kid mode (details at: https://emcot.world/index.php/Disney_Bluetooth_Wands)
+bool blackout = false;
+
+int lastmode = 0;
+int timer = 0;
+
+BLEScan *pBLEScan; //Old code but compatible
+
+int selected_color = 0;
+int last_color = 0;
+
+ESP32Time rtc(3600);  //Realtime clock for logging
+const int chipSelect = 21;
+char savefilename[30] = "/datalog_00.txt";
+int session_number = 0;
+
+File dataFile;
+
 //Hat and Dress are GRB
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define ENDIAN_CHANGE_U16(x) ((((x)&0xFF00) >> 8) + (((x)&0xFF) << 8))
-
+//https://emcot.world/Disney_MagicBand%2B_Bluetooth_Codes
 uint32_t pallet_array[] = { 
   strip.Color(0, 255, 255),      //0 = Cyan
-  strip.Color(30, 255, 255),      //1 = Purple
+  strip.Color(30, 255, 255),     //1 = Purple
   strip.Color(0, 0, 255),        //2 = Blue
   strip.Color(0, 0, 128),        //3 = Midnight Blue
   strip.Color(0, 0, 255),        //4 = Blue 
-  strip.Color(255, 60, 255),      //5 = bright purple
-  strip.Color(153, 0, 153),       //6 = 
-  strip.Color(153, 0, 153),         //7
-  strip.Color(153, 0, 153),        //8
-  strip.Color(153, 0, 153),        //9
-  strip.Color(153, 0, 153),       //10
-  strip.Color(153, 0, 153),      //11
-  strip.Color(153, 0, 153),       //12   13 20   25    4 1
-  strip.Color(153, 0, 153),      //13 uurplr
-  strip.Color(153, 0, 153),        //14   255,170,0 = Good Yellow
-  strip.Color(255, 60, 0),       //15 off YUellow was 255 255 33
-  strip.Color(255, 255, 128),      //16 Off Yellow
+  strip.Color(255, 60, 255),     //5 = bright purple
+  strip.Color(153, 0, 153),      //6 = lavender
+  strip.Color(153, 0, 153),      //7 = purple
+  strip.Color(195, 122, 123),      //8 = pink
+  strip.Color(195, 122, 123),     //9 = pink
+  strip.Color(195, 122, 123),      //10 = pink
+  strip.Color(195, 122, 123),      //11 = pink
+  strip.Color(195, 122, 123),      //12 = pink
+  strip.Color(195, 122, 123),      //13 = pink
+  strip.Color(195, 122, 123),      //14 = pink
+  strip.Color(255, 60, 0),       //15 = Yellow Orange
+  strip.Color(255, 255, 128),    //16 Off Yellow
   strip.Color(255, 200, 50),     //17 Yellow ORange
-  strip.Color(255, 255, 0),                                   //18 Lime Greenstrip.Color(51, 255, 51), 
-  strip.Color(255, 30, 0),       //19 Orange
-  strip.Color(255, 10, 0),        //20 Red Orange?
-  strip.Color(255, 0, 0),         //21 Red 
-  strip.Color(0, 255, 255),
-  strip.Color(0, 255, 255),
-  strip.Color(0, 255, 255),
+  strip.Color(51, 255, 51), 	 //18 Lime Greenstrip.Color(51, 255, 51), 
+  strip.Color(255, 50, 0),       //19 Orange
+  strip.Color(255, 30, 0),       //20 Red Orange?
+  strip.Color(255, 0, 0),        //21 Red 
+  strip.Color(0, 255, 255),		 //22 cyan
+  strip.Color(0, 255, 255),		  //23 cyan
+  strip.Color(0, 255, 255),		//24 cyan
   strip.Color(0, 255, 0),         //25 Green
-  strip.Color(0, 0, 255),  //27
-  strip.Color(255, 255, 255),
-  strip.Color(255, 255, 255),
-  strip.Color(0, 0, 0),
-  strip.Color(0, 0, 0),
-  strip.Color(255, 255, 255)      //32 White                   
+  strip.Color(0, 255, 51),  		//26 Lime Green
+  strip.Color(255, 255, 255),		//27 White
+  strip.Color(255, 255, 255),		//28 white
+  strip.Color(0, 0, 0),				//29 Off
+  strip.Color(0, 0, 0),				//unique
+  strip.Color(255, 255, 255)      //Random                   
 };
 
-
+// This is used by the bluetooth wand.
+// More colors can be added.
 uint32_t mode_array[] = {
   strip.Color(0,0,255),
   strip.Color(0,255,0),
@@ -87,41 +109,36 @@ uint32_t mode_array[] = {
   strip.Color(255, 102, 0)
 };  
 
-
+// Temporary Variables for pattern colors
 uint32_t temp_color_array[] = { strip.Color(0, 0, 0), strip.Color(0, 0, 0), strip.Color(0, 0, 0), strip.Color(0, 0, 0), strip.Color(0, 0, 0) };
-
 int temp_start = 0;
 int temp_x = 0;
 int temp_y = 0;
 int temp_z = 0;
-
-
-hw_timer_t *My_timer = NULL;
-
   
 int led1 = 0;
 int led2 = 0;
 int led3 = 0;
 int led4 = 0;
 int led5 = 0;
-                          
 
+hw_timer_t *My_timer = NULL;
 
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\n", dirname);
-
+int listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    //Serial.printf("Listing directory: %s\n", dirname);
     File root = fs.open(dirname);
     if(!root){
         Serial.println("Failed to open directory");
-        return;
+        return 9999;
     }
     if(!root.isDirectory()){
         Serial.println("Not a directory");
-        return;
+        return 9999;
     }
-
+    int filecount = 0;
     File file = root.openNextFile();
     while(file){
+        filecount++;
         if(file.isDirectory()){
             Serial.print("  DIR : ");
             Serial.println(file.name());
@@ -136,42 +153,11 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
         }
         file = root.openNextFile();
     }
+    return filecount;
 }
 
-void createDir(fs::FS &fs, const char * path){
-    Serial.printf("Creating Dir: %s\n", path);
-    if(fs.mkdir(path)){
-        Serial.println("Dir created");
-    } else {
-        Serial.println("mkdir failed");
-    }
-}
 
-void removeDir(fs::FS &fs, const char * path){
-    Serial.printf("Removing Dir: %s\n", path);
-    if(fs.rmdir(path)){
-        Serial.println("Dir removed");
-    } else {
-        Serial.println("rmdir failed");
-    }
-}
-
-void readFile(fs::FS &fs, const char * path){
-    Serial.printf("Reading file: %s\n", path);
-
-    File file = fs.open(path);
-    if(!file){
-        Serial.println("Failed to open file for reading");
-        return;
-    }
-
-    Serial.print("Read from file: ");
-    while(file.available()){
-        Serial.write(file.read());
-    }
-    file.close();
-}
-
+// Use to create the file.
 void writeFile(fs::FS &fs, const char * path, const char * message){
     Serial.printf("Writing file: %s\n", path);
 
@@ -187,7 +173,6 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
     }
     file.close();
 }
-
 void appendFile(fs::FS &fs, const char * path, const char * message){
     Serial.printf("Appending to file: %s\n", path);
 
@@ -204,91 +189,7 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
     file.close();
 }
 
-void renameFile(fs::FS &fs, const char * path1, const char * path2){
-    Serial.printf("Renaming file %s to %s\n", path1, path2);
-    if (fs.rename(path1, path2)) {
-        Serial.println("File renamed");
-    } else {
-        Serial.println("Rename failed");
-    }
-}
 
-void deleteFile(fs::FS &fs, const char * path){
-    Serial.printf("Deleting file: %s\n", path);
-    if(fs.remove(path)){
-        Serial.println("File deleted");
-    } else {
-        Serial.println("Delete failed");
-    }
-}
-
-void testFileIO(fs::FS &fs, const char * path){
-    File file = fs.open(path);
-    static uint8_t buf[512];
-    size_t len = 0;
-    uint32_t start = millis();
-    uint32_t end = start;
-    if(file){
-        len = file.size();
-        size_t flen = len;
-        start = millis();
-        while(len){
-            size_t toRead = len;
-            if(toRead > 512){
-                toRead = 512;
-            }
-            file.read(buf, toRead);
-            len -= toRead;
-        }
-        end = millis() - start;
-        Serial.printf("%u bytes read for %u ms\n", flen, end);
-        file.close();
-    } else {
-        Serial.println("Failed to open file for reading");
-    }
-
-
-    file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-
-    size_t i;
-    start = millis();
-    for(i=0; i<2048; i++){
-        file.write(buf, 512);
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-    file.close();
-}
-
-int scanTime = 1;  //In seconds
-int sequenceNumber = 0;
-int mode = 1;  
-int dressmode = 0; //Default to Kid mode (details at: https://emcot.world/index.php/Disney_Bluetooth_Wands)
-bool blackout = false;
-
-int lastmode = 0;
-int timer = 0;
-
-BLEScan *pBLEScan; //Old code but compatible
-
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c "
-#define BYTE_TO_BINARY(byte) \
-  (byte & 0x80 ? '1' : '0'), \
-    (byte & 0x40 ? '1' : '0'), \
-    (byte & 0x20 ? '1' : '0'), \
-    (byte & 0x10 ? '1' : '0'), \
-    (byte & 0x08 ? '1' : '0'), \
-    (byte & 0x04 ? '1' : '0'), \
-    (byte & 0x02 ? '1' : '0'), \
-    (byte & 0x01 ? '1' : '0')
-
-//
-int selected_color = 0;
-int last_color = 0;
 
 
 //This is the Bluetooth scanner... Listening for all the fun stuff.
